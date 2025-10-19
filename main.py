7438a16b8872cc10326fe220b8e521c73538a0da
David Birks <david@birks.dev>
2025-10-18 23:15:02 -0400

    feat: Add basic head movement example

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from reachy_mini import ReachyMini
+from reachy_mini.utils import create_head_pose
+
+
 def main():
     print("Hello from reachy-robot!")
+
+    with ReachyMini() as r:
+        r.goto_target(
+            head=create_head_pose(z=10, roll=15, degrees=True, mm=True), duration=2.0
+        )
+        r.goto_target(head=create_head_pose(), duration=2.0)
 
 
 if __name__ == "__main__":
